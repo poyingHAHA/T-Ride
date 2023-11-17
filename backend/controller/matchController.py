c409628687dc50a45b770545cfb263ca88050ca6
Sam <samwang910325@gmail.com>
2023-11-17 19:59:14 +0800

    tmp

--- backend/controller/matchController.py
+++ backend/controller/matchController.py
@@ -1,133 +1,160 @@
-from flask import Blueprint
-from services.match import *
+from quart import Blueprint
+from services.matchService import *
 
 match = Blueprint('match_page', __name__)
 
-@match.route('/driver/invitation', methods=['POST'])
-def post_driver_invitation():
-  """
-    司機發邀請給乘客
-    ---
-    parameters:
-    - name: token
-      in: requestBody
-      type: string
-      required: true
-      description: 認證司機身分所使用
-    - name: driverOrderId
-      in: requestBody
-      type: integer
-      required: true
-      description: 司機訂單
-    - name: passengerOrderId
-      in: requestBody 
-      type: integer
-      required: true
-      description: 乘客訂單
-    responses:
-      200:
-        description: 成功送出邀請
-      401:
-        description: token 無效 
-      403:
-        description: 不是自己的駕駛訂單 
-      404:
-        description: 不存在的訂單
-      409:
-        description: 已發過邀請、已接受、已完成的訂單
-  """
-  return send_driver_invitation() 
+# @match.route('/driver', methods=['POST'])
+# async def post_driver_order():
+#   """
+#     司機發邀請給乘客
+#     ---
+#     parameters:
+#     - name: token
+#       in: requestBody
+#       type: string
+#       required: true
+#       description: 認證司機身分所使用
+#     - name: startName
+#       in: requestBody 
+#       type: string
+#       required: true
+#       description: 上車地點名稱
+#     - name: startPoint
+#       in: requestBody
+#       type: string
+#       required: true
+#       description: 上車點的經緯度座標(format:"經度,緯度")
+#     - name: endName
+#       in: requestBody 
+#       type: string
+#       required: true
+#       description: 下車地點名稱
+#     - name: endPoint
+#       in: requestBody
+#       type: string
+#       required: true
+#       description: 下車點的經緯度座標(format:"經度,緯度")
+#     - name: departureTi,e
+#       in: requestBody
+#       type: integer
+#       required: true
+#       description: 下單時間(unix time seconds)
+#     - name: passengerCount
+#       in: requestBody
+#       type: string
+#       required: true
+#       description: 司機可載的人數
+#     definitions:
+#       BookOrderResponse:
+#         type: object
+#         properties:
+#           orderId:
+#             type: integer
+#     responses:
+#       200:
+#         description: 成功刪除司機給乘客的邀請
+#         schema:
+#           $ref: '#/definitions/BookOrderResponse' 
+#       401:
+#         description: token 無效 
+#       403:
+#         description: 不是自己的駕駛訂單 
+#       404:
+#         description: 不存在的訂單
+#       409:
+#         description: 已發過邀請、已接受、已完成的訂單
+#   """
+#   return "not implemented"
 
-@match.route('/driver/invitation', methods=['DELETE'])
-def delete_driver_invitation():
-    """
-    刪除司機送給乘客的邀請
-    ---
-    parameters:
-    - name: token
-      in: requestBody
-      type: string
-      required: true
-      description: 認證身分使用
-    - name: driverOrderId
-      in: requestBody
-      type: integer
-      required: true
-      description: 司機訂單
-    - name: passengerOrderId
-      in: requestBody 
-      type: integer
-      required: true
-      description: 乘客訂單
-    definitions:
-      cancelResponse:
-        type: object
-        properties:
-          totalOrderCount:
-            type: integer
-          abandonCount:
-            type: integer
-    responses:
-      200:
-        description: 成功刪除司機給乘客的邀請
-        schema:
-          $ref: '#/definitions/cancelResponse' 
-      401:
-        description: token 無效 
-      403:
-        description: 不是自己的駕駛訂單 
-      404:
-        description: 不存在的訂單
-      409:
-        description: 已發過邀請、已接受、已完成的訂單
-    """
-    send_driver_invitation() 
+# @match.route('/driver/invitation', methods=['DELETE'])
+# async def delete_driver_invitation():
+#     """
+#     刪除司機送給乘客的邀請
+#     ---
+#     parameters:
+#     - name: token
+#       in: requestBody
+#       type: string
+#       required: true
+#       description: 認證身分使用
+#     - name: driverOrderId
+#       in: requestBody
+#       type: integer
+#       required: true
+#       description: 司機訂單
+#     - name: passengerOrderId
+#       in: requestBody 
+#       type: integer
+#       required: true
+#       description: 乘客訂單
+#     definitions:
+#       cancelResponse:
+#         type: object
+#         properties:
+#           totalOrderCount:
+#             type: integer
+#           abandonCount:
+#             type: integer
+#     responses:
+#       200:
+#         description: 成功刪除司機給乘客的邀請
+#         schema:
+#           $ref: '#/definitions/cancelResponse' 
+#       401:
+#         description: token 無效 
+#       403:
+#         description: 不是自己的駕駛訂單 
+#       404:
+#         description: 不存在的訂單
+#       409:
+#         description: 已發過邀請、已接受、已完成的訂單
+#     """
+#     send_driver_invitation() 
 
-
-@match.route('/driver/invitation/total/<int:driverOrderId>', methods=['GET'])
-def get_driver_all_invitations(driverOrderId):
-    """
-    發送邀請
-    ---
-    parameters:
-      - name: driverOrderId
-        in: path
-        type: integer
-        required: true
-        description: 司機訂單
-    definitions:
-      Palette:
-        type: object
-        properties:
-          palette_name:
-            type: array
-            items:
-              $ref: '#/definitions/Color'
-      Color:
-        type: string
-    responses:
-      200:
-        description: 成功刪除
-        schema:
-          $ref: '#/definitions/Palette'
-        examples:
-          rgb: ['red', 'green', 'blue']
-      401:
-        description: token 無效 
-      403:
-        description: 不是自己的駕駛訂單 
-      404:
-        description: 不存在的訂單
-      409:
-        description: 已發過邀請、已接受、已完成的訂單
-    components:
-      schemas:
-        User:
-        type: object
-        properties:
-            id:
-            type: integer
-            format: int64
-            example: 10
-    """
-    return ""
+# @match.route('/driver/invitation/total/<int:driverOrderId>', methods=['GET'])
+# async def get_driver_all_invitations(driverOrderId):
+#     """
+#     發送邀請
+#     ---
+#     parameters:
+#       - name: driverOrderId
+#         in: path
+#         type: integer
+#         required: true
+#         description: 司機訂單
+#     definitions:
+#       InvitationResponses:
+#         type: object
+#         properties:
+#           palette_name:
+#             type: array
+#             items:
+#               $ref: '#/definitions/Color'
+#       Color:
+#         type: string
+#     responses:
+#       200:
+#         description: 成功刪除
+#         schema:
+#           $ref: '#/definitions/Palette'
+#         examples:
+#           rgb: ['red', 'green', 'blue']
+#       401:
+#         description: token 無效 
+#       403:
+#         description: 不是自己的駕駛訂單 
+#       404:
+#         description: 不存在的訂單
+#       409:
+#         description: 已發過邀請、已接受、已完成的訂單
+#     components:
+#       schemas:
+#         User:
+#         type: object
+#         properties:
+#             id:
+#             type: integer
+#             format: int64
+#             example: 10
+#     """
+#     return ""
--- backend/controller/userController.py
+++ backend/controller/userController.py
@@ -6,13 +6,13 @@ user = Blueprint('user_page', __name__)
 @user.route('/login', methods=['POST'])
 async def user_login():
     payload = await request.get_data()
-    return login()
+    return "NOT IMPLEMENTED"
 
 @user.route('/<int:userId>', methods=['GET'])
 async def get_driver_data(userId):
-    return collect_user_data(userId)
+    return "NOT IMPLEMENTED"
 
 @user.route('/driver_data', methods=['POST'])
 async def post_driver_data():
     payload = await request.get_data()
-    return update_user_data(payload)
+    return "NOT IMPLEMENTED"

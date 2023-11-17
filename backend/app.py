5312cb5949a052a39448bd7419e40e36c763b444
Sam <samwang910325@gmail.com>
2023-11-17 21:19:54 +0800

    feat: 新增swagger

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from quart import Quart
+from quart import Quart, send_from_directory
 import subprocess
 from controller.matchController import match
 from controller.orderController import order
@@ -18,8 +18,19 @@ async def github_webhook():
     subprocess.run(['git', 'pull'])
     return 'OK'
 
+@app.route('/Swagger/')
+@app.route('/Swagger/<path:path>')
+async def swagger(path=None):
+    folder_path = 'Swagger'
+    file_name = 'index.html'
+
+    if path is None:
+        return await send_from_directory(folder_path, file_name)
+
+    return await send_from_directory(folder_path, path)
+
 if __name__ == '__main__':
-    app.register_blueprint(match, url_prefix='/match')
-    app.register_blueprint(order, url_prefix='/order')
+    #app.register_blueprint(match, url_prefix='/match')
+    #app.register_blueprint(order, url_prefix='/order')
     app.register_blueprint(user, url_prefix='/user')
     app.run(host='0.0.0.0', port=5239, debug=True)

b5ddb41754e7d97fe270dd178507352225b37d31
poyingHAHA <poying.code@gmail.com>
2023-11-23 09:30:03 +0800

    ADD slices/driverStartDest.ts 紀錄司機起點終點

---
+++ frontend/src/slices/driverStartDest.ts
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface driverStartDestState {
+  start: {
+    name?: string,
+    placeId?: string,
+    lat?: number,
+    lng?: number
+  },
+  dest: {
+    name?: string,
+    placeId?: string,
+    lat?: number,
+    lng?: number
+  }
+}
+
+const initialState: driverStartDestState = {
+  start: {
+    name: undefined,
+    placeId: undefined,
+    lat: undefined,
+    lng: undefined
+  },
+  dest: {
+    name: undefined,
+    placeId: undefined,
+    lat: undefined,
+    lng: undefined
+  }
+}
+
+export const driverStartDest = createSlice({
+  name: 'driverStartDest',
+  initialState,
+  reducers: {
+    setStart: (state, action) => {
+      state.start.name = action.payload.name
+      state.start.placeId = action.payload.placeId
+      state.start.lat = action.payload.lat
+      state.start.lng = action.payload.getLatLng
+    },
+    setDest: (state, action) => {
+      state.dest.name = action.payload.name
+      state.dest.placeId = action.payload.placeId
+      state.dest.lat = action.payload.lat
+      state.dest.lng = action.payload.getLatLng
+    }
+  }
+})
+
+export const { setStart, setDest } = driverStartDest.actions;
+export default driverStartDest.reducer;
+  

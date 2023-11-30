8d068cd877436528ab4c02293e8e9f365a044254
poyingHAHA <poying.code@gmail.com>
2023-11-30 16:59:16 +0800

    FIX 修復userLogin post問題

--- frontend/src/services/loginService.ts
+++ frontend/src/services/loginService.ts
@@ -7,7 +7,15 @@ interface LoginRequest {
 
 const userLogin = async (params: LoginRequest) => {
   try {
-    const response = await post(`/user/login`, JSON.stringify(params));
+    const response = await post(
+      `/user/login`, 
+      JSON.stringify(params),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
     console.log("login -> login response: ", response);
     return response;
   } catch (error) {

d49805ea1b06f85e008f6f914745ebc756e42875
Archie <archie.weichi@gmail.com>
2023-12-10 19:15:41 +0800

    modify driverTrip, driverDetail

--- frontend/src/pages/Driver/pages/DriverTrip.tsx
+++ frontend/src/pages/Driver/pages/DriverTrip.tsx
@@ -7,6 +7,7 @@ import DriverTripCard from "../components/DriverTripCard";
 import { useAppSelector, useAppDispatch } from "../../../hooks";
 import { getStartEnd, getInvitationTotal  } from "../../../services/driveOrderService";
 import { StartEnd, InfoItem, setStartEnd, setJourney } from "../../../slices/driverJourney";
+import ErrorLoading from '../../../components/ErrorLoading';
 
 
 interface TripItem {
@@ -16,6 +17,9 @@ interface TripItem {
 }
 
 const DriverTrip: React.FC =() => {
+
+  //Todo:抓orderId
+
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const location = useLocation();
@@ -24,12 +28,38 @@ const DriverTrip: React.FC =() => {
   const [orderId, setOrderId] = useState(order);
   const [trip, setTrip] = useState<TripItem[]>([]);
   const [isLoad, setIsLoad] = useState(false);
+  const [date, setDate] = useState("");
+  const [loading, setLoading] = useState<boolean>(false); //ErrorLoading
+  const [error, setError] = useState<string>("");
+
+  //navigation
+  const handleNavigate = () => {
+    const start = [driverJourneyReducer.StartPoint.place.lat, driverJourneyReducer.StartPoint.place.lng]
+    const mid = driverJourneyReducer.Midpoints.map((mid) => {
+      const location = [mid.startPlace.lat, mid.startPlace.lng];
+      return location;
+    });
+    const end = [driverJourneyReducer.EndPoint.place.lat, driverJourneyReducer.EndPoint.place.lng]
+    const coordinates = [start, ...mid, end];
+
+    const origin = coordinates[0];
+    const waypoints = coordinates.slice(1);
+    const baseUrl = "https://www.google.com/maps/dir/";
+    const urlEnd = `/@${origin},16z/`; 
+    // /data=!4m2!4m1!3e0?authuser=0&entry=ttu
+    // /data=!3m1!4b1?entry=ttu
+    const waypointsString = waypoints.map(coord => coord.join(',')).join('/');
+    const externalURL = `${baseUrl}${origin}/${waypointsString}${urlEnd}`;
+    window.location.href = externalURL;
+  };
 
   useEffect(() => {
     async function fetchAll() {
       try {
-        const middle: InfoItem[] = await getInvitationTotal(orderId) as InfoItem[];
-        dispatch(setJourney(middle));
+        // setLoading(true);
+        // const middle: InfoItem[] = await getInvitationTotal(orderId) as InfoItem[];
+        // setLoading(false);
+        // dispatch(setJourney(middle));
         const mappedPlaces = driverJourneyReducer.Midpoints.flatMap((mid) => [
           {
             type: "中途經過",
@@ -42,24 +72,45 @@ const DriverTrip: React.FC =() => {
             time: mid.arriveTime,
           }
         ]);
-        const StartEnd: StartEnd[] = await getStartEnd(orderId) as StartEnd[];
-        dispatch(setStartEnd(StartEnd));
-        const start = { type: "起點", name: driverJourneyReducer.StartPoint.name, time: driverJourneyReducer.StartPoint.time };
-        const end = { type: "終點", name: driverJourneyReducer.EndPoint.name, time: driverJourneyReducer.EndPoint.time };
 
+        // Sorting based on time
+        mappedPlaces.sort((a:TripItem, b:TripItem) => {
+          const timeA:any = new Date("1970-01-01T" + a.time);  
+          const timeB:any = new Date("1970-01-01T" + b.time);
+          return timeA - timeB;  
+        });
+
+        // setLoading(true);
+        // const StartEnd: StartEnd[] = await getStartEnd(orderId) as StartEnd[];
+        // setLoading(false);
+        // dispatch(setStartEnd(StartEnd));
+        const start = { 
+          type: "起點", 
+          name: driverJourneyReducer.StartPoint.name, 
+          time: driverJourneyReducer.StartPoint.time, 
+        };
+        setDate(driverJourneyReducer.StartPoint.date );
+        const end = { 
+          type: "終點", 
+          name: driverJourneyReducer.EndPoint.name, 
+          time: driverJourneyReducer.EndPoint.time, 
+        };
         setTrip([start, ...mappedPlaces, end]);
         setIsLoad(true);
       } catch (error) {
         console.log(error);
+        setLoading(false);
+        setError("發生錯誤");
       }
     }
     fetchAll();
   }, [isLoad]);
 
+
   return (
-    <div className="bg-[#f3e779] m-0 h-full w-screen">
+    <div className="bg-[#ededed] m-0 h-full w-screen">
       <div className="text-center w-screen fixed top-[60px] font-bold text-[36px] font-serif">行程資訊</div>
-      <div className="text-center w-screen fixed top-[120px] font-normal text-[18px] font-sans">2023年11月2日 星期三</div>
+      <div className="text-center w-screen fixed top-[120px] font-normal text-[18px] font-sans">{date}</div>
       <div className="flex flex-col items-center rounded-t-[30px] bg-white w-screen h-[calc(100vh-320px)] fixed bottom-[150px] overflow-auto pt-[40px] pb-[40px] gap-[30px]">
         {trip.map((place, index) => {
           return (<DriverTripCard key={index} {...place} />);
@@ -67,24 +118,28 @@ const DriverTrip: React.FC =() => {
       </div>
       <div className="bg-white flex flex-row justify-evenly items-center w-screen h-[100px] fixed bottom-[70px]">
           <button
-            className="w-[50px] h-[50px] bg-[#f3e779] rounded-[25px] flex justify-center items-center"
+            className="w-[60px] h-[60px] bg-black rounded-[10px] flex justify-evenly items-center"
             onClick={() => {
               navigate("/driver/detail", {state: {orderId: orderId}})
-            }}
-          ><IoIosInformationCircleOutline className="w-[40px] h-[40px]"/></button>
+            }}>
+            <IoIosInformationCircleOutline className="w-[40px] h-[40px] text-white"/>
+          </button>
           <button
-            className="w-[150px] h-[50px] rounded-[10px] bg-[#2e5a88] text-white text-[24px]"
+            className="w-[150px] h-[60px] bg-black rounded-[10px] flex justify-evenly items-center"
             onClick={() => {
-              navigate("/driver/navigate")
-            }}
-          >開始導航</button>
+              handleNavigate();
+            }}>
+            <span className="text-[24px] text-white">導航</span>
+          </button>
           <button 
-            className="w-[50px] h-[50px] rounded-[10px] bg-[#f3e779] flex justify-center items-center rounded-[25px]"
+            className="w-[60px] h-[60px] rounded-[10px] bg-black flex justify-evenly items-center rounded-[10px]"
             onClick={() => {
               navigate("/driver/rating", {state: {orderId: orderId}})
-            }}
-            ><IoStarSharp className="w-[30px] h-[30px]"/></button>
+            }}>
+              <IoStarSharp className="w-[30px] h-[30px] text-white"/>
+            </button>
       </div>
+      <ErrorLoading error={error} setError={setError} loading={loading} setLoading={setLoading} />
     </div>
   );
 }
--- frontend/src/services/driveOrderService.ts
+++ frontend/src/services/driveOrderService.ts
@@ -1,7 +1,7 @@
-import { get, post } from './APIHelper';
+import { get, post, remove } from './APIHelper';
 import { driverOrderDTO, driverInvitationDTO, driverInvitationTotalDTO, orderDTO } from '../DTO/orders';
 import { userDTO } from '../DTO/user';
-import { convertUTC } from './formatService';
+import { convertUTC, convertDate } from './formatService';
 import { getUserId } from '../utils/userUtil';
 
 interface PostDriverOrderRequest {
@@ -49,6 +49,24 @@ const getDriverUnfinishedOrder = async () => {
   }
 }
 
+const deleteDriverInvitation = async (driverOrderId: number, passengerOrderId: number, token: string) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    params: {
+      token: token
+    }
+  };
+  try {
+    const response = await remove(`/match/driver/invitation/${driverOrderId}/${passengerOrderId}`, config);
+    console.log("deleteDriverInvitation", response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 const getStartEnd = async (orderId: number) => {
   try {
     const response: {data: driverOrderDTO} = await get(`/order/driver/${orderId}`) as {data: driverOrderDTO};
@@ -58,14 +76,16 @@ const getStartEnd = async (orderId: number) => {
         place: {
           lat: response.data.startPoint.lat,
           lng: response.data.startPoint.lng
-        } 
+        },
+        date: convertDate(response.data.departureTime)
       },
       { name: response.data.endName, 
         time: "",
         place: {
           lat: response.data.endPoint.lat,
           lng: response.data.startPoint.lng
-        }
+        },
+        date: ""
       }
     ];
     return startEnd;
@@ -91,6 +111,7 @@ const getInvitationTotal = async (orderId: number) => {
       .map(async (order) => {
         const name: string = await getUserName(order.passengerOrder.userId);
         return {
+          orderId: order.passengerOrder.orderId,
           userId: order.passengerOrder.userId,
           userName: name,
           startName: order.passengerOrder.startName,
@@ -107,7 +128,8 @@ const getInvitationTotal = async (orderId: number) => {
             lng: order.passengerOrder.endPoint.lng
           },
           fee: order.passengerOrder.fee,
-          passengerCount: order.passengerOrder.passengerCount
+          passengerCount: order.passengerOrder.passengerCount,
+          date: convertDate(order.passengerOrder.arrivalTime)
         };
       }));
     return acceptedOrders;
@@ -116,5 +138,14 @@ const getInvitationTotal = async (orderId: number) => {
   }
 }
 
-export { postDriverOrder, getStartEnd, getInvitationTotal, getDriverUnfinishedOrder };
+const getPerson = async (userId: number) => {
+  try {
+    const response:{data: userDTO} = await get(`/user/${userId}`) as {data: userDTO};
+    return response.data;
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+export { postDriverOrder, getStartEnd, getInvitationTotal, getDriverUnfinishedOrder, deleteDriverInvitation, getPerson };
 

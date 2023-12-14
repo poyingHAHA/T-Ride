cd0fce44d5e433ecc8f4c008301b550303e24af8
poyingHAHA <poying.code@gmail.com>
2023-12-14 19:56:53 +0800

    FIX 登出後清除所有state

--- frontend/src/pages/Driver/pages/DriverSetting.tsx
+++ frontend/src/pages/Driver/pages/DriverSetting.tsx
@@ -1,9 +1,18 @@
 import { MdAccountCircle } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { UnsetCookie, getTokenFromCookie } from '../../../utils/cookieUtil';
+import { useDispatch } from "react-redux";
+import { resetState } from "../../../store";
 
 export default function DriverSetting() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const logoutHandler = () => {
+        UnsetCookie();
+        dispatch(resetState());
+        navigate("/");
+    }
 
     return (
         <div className='flex flex-col'>
@@ -20,15 +29,10 @@ export default function DriverSetting() {
 
                 <div className="group">
                     <button className="flex w-full items-center px-6 py-5 group-hover:bg-gray-100"
-                        onClick={() => {
-                            navigate("/");
-                            UnsetCookie();
-                        }}>
+                        onClick={logoutHandler}>
                         <div className="text-neutral-800 text-red-500 dark:text-white text-lg">Log out</div>
                     </button>
                 </div>
-
-
             </div>
         </div >
     )

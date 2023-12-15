184399fd16f3c3b9d00356878be634bcdf0fe9c1
jasonhu <jasonhu1012@hotmail.com.tw>
2023-12-15 10:08:36 +0800

    feat: 新增config指定要不要註冊port

--- backend/api/services/notificationService.py
+++ backend/api/services/notificationService.py
@@ -2,7 +2,7 @@ from repository.notificationRepository import *
 from repository.userRepository import *
 from repository.orderRepository import *
 from repository.matchRepository import *
-import re
+from utils.config import Config
 
 
 class NotificationService:
@@ -14,13 +14,15 @@ class NotificationService:
 
     def register_host_port(self, user_id, host_port):
         '''
-        return "user not found",
-               "invalid format"
+        return "user not found"
 
         return None on success
         '''
         if self.user_repository.get_user(user_id) is None:
             return "user not found"
+
+        if not bool(int(Config.get('service').get('websocket_register_port'))):
+            host_port = host_port.split(':')[0]
 
         self.notification_repository.register_host_port(user_id, host_port)
 
